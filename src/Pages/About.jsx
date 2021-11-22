9bd524bd2b015bab851825f9326453929c98fb4d
waldofx <90760767+waldofx@users.noreply.github.com>
2021-11-22 17:53:18 +0700

    About basic
    the backgrounds for about page is done, still has problem with the text though.

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -8,7 +8,18 @@ function About() {
     return (
         <div>
             <Header />
-            <div></div>
+            <div className={`${classes["bg-img"]} ${classes["about"]}`}>
+                <div className={classes["bg-text"]}>
+                    ABOUT
+                    <div>BRUH BRUH BRUH BRUH BRUH BRUH BRUH BURH</div>
+                </div>
+            </div>
+            <div className={`${classes["bg-img"]} ${classes["problem"]}`}>
+                <div className={classes["bg-text"]}>PROBLEM</div>
+            </div>
+            <div className={`${classes["bg-img"]} ${classes["solution"]}`}>
+                <div className={classes["bg-text"]}>SOLUTION</div>
+            </div>
             <Footer />
         </div>
     );
