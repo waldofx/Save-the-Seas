816a3fc533811056f53adb6617f4bebb3e58afbe
waldofx <90760767+waldofx@users.noreply.github.com>
2021-11-23 16:18:08 +0700

    clean up

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,24 +1,28 @@
 import React from "react";
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
-import classes from "./About.module.css";
+import styles from "./About.module.css";
 // import { Link } from "react-router-dom";
 
 function About() {
     return (
         <div>
             <Header />
-            <div className={`${classes["bg-img"]} ${classes["about"]}`}>
-                <div className={classes["bg-text"]}>
+            <div className={`${styles["bg-img"]} ${styles["about"]}`}>
+                <div className={styles["bg-text"]}>
                     ABOUT
                     <div>BRUH BRUH BRUH BRUH BRUH BRUH BRUH BURH</div>
                 </div>
             </div>
-            <div className={`${classes["bg-img"]} ${classes["problem"]}`}>
-                <div className={classes["bg-text"]}>PROBLEM</div>
+            <div className={`${styles["bg-img"]} ${styles["problem"]}`}>
+                <div className={`${styles["bg-text"]} ${styles["problem2"]}`}>
+                    PROBLEM
+                </div>
             </div>
-            <div className={`${classes["bg-img"]} ${classes["solution"]}`}>
-                <div className={classes["bg-text"]}>SOLUTION</div>
+            <div className={`${styles["bg-img"]} ${styles["solution"]}`}>
+                <div className={`${styles["bg-text"]} ${styles["solution2"]}`}>
+                    SOLUTION
+                </div>
             </div>
             <Footer />
         </div>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import Header from "../Components/Header";
-import classes from "./Home.module.css";
+import styles from "./Home.module.css";
 import { Link } from "react-router-dom";
 
 function Home() {
     return (
         <div>
             <Header />
-            <div className={classes.bgImg}></div>
-            <div className={classes.bgText}>
-                <h1 className={`${classes["text"]} ${classes["text-title"]}`}>
+            <div className={styles.bgImg}></div>
+            <div className={styles.bgText}>
+                <h1 className={`${styles["text"]} ${styles["text-title"]}`}>
                     Save The Seas
                 </h1>
-                <p className={`${classes["text"]} ${classes["text-md"]}`}>
+                <p className={`${styles["text"]} ${styles["text-md"]}`}>
                     Help us save the seas by cleaning up the trash left behind!
                 </p>
                 <Link to="/about">
-                    <button data-test-id="btn" className={classes.btn}>
+                    <button data-test-id="btn" className={styles.btn}>
                         Learn More
                     </button>
                 </Link>
